54e5e48488ff0791aaf1aa9f8438dcd0012aad4e
Peter Ditlev Kristensen <petdk@ramboll.com>
2026-05-18 16:15:26 +0200

    deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubPages ? "/stamper" : "",
+  assetPrefix: isGithubPages ? "/stamper/" : "",
 };
 
 export default nextConfig;
